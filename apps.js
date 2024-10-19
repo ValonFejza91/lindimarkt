b0931743de13321065419addd394bfa17035eca3
Valon Fejza <130112033+ValonFejza91@users.noreply.github.com>
2024-10-19 14:24:35 +0200

    Update apps.js
    coockies added

--- apps.js
+++ apps.js
@@ -173,14 +173,16 @@ function SendMail() {
 }
 
 
+//Coockies
+
 window.onload = function() {
   if (!localStorage.getItem('cookiesAccepted')) {
     document.getElementById('cookie-banner').style.display = 'block';
   }
 };
 
-document.getElementById('accept-cookies').onclick = function() {
+document.getElementById('accept-cookies').addEventListener('click', function() {
   localStorage.setItem('cookiesAccepted', 'true');
   document.getElementById('cookie-banner').style.display = 'none';
-};
+});
 
